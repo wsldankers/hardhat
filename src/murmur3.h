2519151a6510e2dd559b553e9d39cff8d2c88e97
Wessel Dankers <wsl@fruit.je>
2013-12-02 12:49:40 +0100

    fix murmur compilation

--- src/murmur3.h
+++ src/murmur3.h
@@ -6,6 +6,8 @@ void murmurhash3_x86_32(const void *key, size_t len, uint32_t seed, void *out);
 void murmurhash3_x86_128(const void *key, size_t len, uint32_t seed, void *out);
 void murmurhash3_x64_128(const void *key, size_t len, uint32_t seed, void *out);
 
+#define murmurhash3_32 murmurhash3_x86_32
+
 #if __SIZEOF_POINTER__ > 4 || defined(__ILP32__)
 #define murmurhash3_128 murmurhash3_x64_128
 #else
