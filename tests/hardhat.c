453bc946c8a8803841a0139438a9364bbfc33a40
Wessel Dankers <wsl@fruit.je>
2016-09-02 16:16:20 +0200

    wrap up testdata script and log driver

--- tests/hardhat.c
+++ tests/hardhat.c
@@ -7,41 +7,7 @@
 
 #include "src/reader.h"
 #include "src/maker.h"
-
-#define TODO "TODO"
-#define SKIP "SKIP"
-
-static unsigned int testcounter;
-
-static bool tap(bool test, const char *modifier, const char *fmt, ...) {
-	va_list ap;
-	printf("%s %u", test ? "ok" : "not ok", ++testcounter);
-	if(fmt) {
-		printf(" - ");
-		va_start(ap, fmt);
-		vprintf(fmt, ap);
-		va_end(ap);
-	}
-	if(modifier)
-		printf(" # %s", modifier);
-	putchar('\n');
-	fflush(stdout);
-	return test;
-}
-
-static void bail(const char *fmt, ...) {
-	va_list ap;
-	printf("Bailing out!");
-	if(fmt) {
-		printf(" ");
-		va_start(ap, fmt);
-		vprintf(fmt, ap);
-		va_end(ap);
-	}
-	putchar('\n');
-	fflush(stdout);
-	exit(1);
-}
+#include "tests/tap.h"
 
 int main(void) {
 	char *filename;
